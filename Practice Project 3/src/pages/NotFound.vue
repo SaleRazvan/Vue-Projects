897bf2f355607ccadcfcee67baf3d7622826a816
SaleRazvan <salerazvan@yahoo.com>
2022-06-13 17:26:04 +0300

    Added the big project

---
+++ Practice Project 3/src/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        Maybe check out all our <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
